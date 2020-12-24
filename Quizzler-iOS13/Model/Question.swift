13e762bd969129b2cdda4c5d5b5448cb146a0b03
Fenty Hall <67518544+fentyhall@users.noreply.github.com>
2020-12-24 14:01:09 -0800

    Add files via upload
    quizzler exercise

---
+++ Quizzler-iOS13/Model/Question.swift
@@ -0,0 +1,21 @@
+//
+//  Question.swift
+//  Quizzler-iOS13
+//
+//  Created by Fenty Muliadi on 12/23/20.
+//  Copyright © 2020 The App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct Question {
+    let text: String
+    let answers: [String]
+    let correctAnswer: String
+    
+    init(q: String, a: [String], correctAnswer: String) {
+        text = q
+        answers = a
+        self.correctAnswer = correctAnswer
+    }
+}
